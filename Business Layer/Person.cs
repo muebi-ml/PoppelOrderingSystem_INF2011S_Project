cf331771c9b31af5618d3fdf38b58e3343af30e2
Mukhethwa Muebi <mbxmuk002@myuct.ac.za>
2022-10-07 21:24:09 +0200

    Add initial Architecture Layers

---
+++ Business Layer/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoppelOrderingSystem_INF2011S_Project.Business_Layer
+{
+    public class Person
+    {
+
+        #region Attributes 
+        string firstName, lastName, phone;
+        #endregion
+
+        #region Property Methods 
+        public string FirstName { 
+            get { return firstName; } 
+            set { firstName = value; } 
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+        #endregion
+    }
+}
---
+++ Database Layer/Database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoppelOrderingSystem_INF2011S_Project.Database_Layer
+{
+    public class Database
+    {
+    }
+}
