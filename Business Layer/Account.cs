1709ef208a879d17fcaee9b1bbb3994176326e32
Mukhethwa Muebi <mbxmuk002@myuct.ac.za>
2022-10-10 21:16:10 +0200

    Added: Entity Classes and their Property methods

---
+++ Business Layer/Account.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoppelOrderingSystem_INF2011S_Project.Business_Layer
+{
+    public class Account: Customer
+    {
+        #region Credit Status enum
+        public enum CreditStatus
+        {
+            UNASSIGNED = 0,
+            OKAY = 1,
+            BLACKLISTED = 2
+        }
+        #endregion
+
+        #region Attributes 
+        private string accountID, accountName;
+        private Collection<Order> orders;
+        private Collection<Payment> payments;
+        private CreditStatus creditStatus;
+        private double creditBalance, creditLimit;
+        #endregion
+
+        #region Constructor
+        public Account ( string accountID, string accountName, string customerID, string personID, string firstName, string lastName ) : base(customerID, personID, firstName, lastName)
+        {
+            this.accountID = accountID;
+            this.accountName = accountName;
+            this.orders = new Collection<Order>(); 
+            this.payments = new Collection<Payment>();
+            this.creditStatus = CreditStatus.UNASSIGNED;
+            this.creditBalance = 0;
+            this.creditLimit = 0;
+            
+        }
+        #endregion
+
+        #region Property Methods
+        public string AccountID
+        {
+            get { return this.accountID; }
+        }
+
+        public string AccountName
+        {
+            get { return accountName; }
+            set { this.accountName = value; }
+        }
+        public CreditStatus Status
+        {
+            get { return this.creditStatus; }
+            set { this.creditStatus = value; }
+        }
+
+        public double CreditBalance
+        {
+            get { return this.creditBalance; }
+            set { this.creditBalance = value; }
+        }
+
+        public double CreditLimit
+        {
+            get { return this.creditLimit; }
+            set {  this.CreditLimit = value; }
+        }
+        #endregion
+    }
+}
---
+++ Business Layer/PickingList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace PoppelOrderingSystem_INF2011S_Project.Business_Layer
+{
+    public class PickingList
+    {
+        #region Attributes 
+        private string pickingListID;
+        private DateTime date;
+        private Collection<Product> products;
+        #endregion
+
+        #region Constructor 
+        public PickingList( string pickingListID )
+        {
+            this.pickingListID = pickingListID;
+            this.date = DateTime.Now;
+            this.products = new Collection<Product>();
+        }
+        #endregion
+
+        #region Property Method 
+        public string PickingListID
+        {
+            get { return pickingListID; }
+        }
+
+        public Collection<Product> Products
+        {
+            get { return products; }
+            set { this.products = value; }
+        }
+        #endregion
+
+    }
+}
