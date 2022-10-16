87e5d73046ac1342406b0f0d49cad3d27c1d1a6e
Mukhethwa Muebi <MBXMUK002@myuct.ac.za>
2022-10-16 06:38:00 +0200

    Update test program

--- Program.cs
+++ Program.cs
@@ -22,24 +22,34 @@ namespace PoppelOrderingSystem_INF2011S_Project
 
             
             CustomerDB db = new CustomerDB();
+            //Console.WriteLine("Creating a new customer....\n");
+            // Test new customer
+            Address address = new Address("22 Rochester Rd", "Observatory", "Cape Town", 7925);
+            Account account = new Account( "Zero Shop", Account.CreditStatus.BLACKLISTED, 1200.99, 2400.75);
+            Customer customer = new Customer("Mukhethwa", "Muebi", "+27606683825", "muebiml1999@gmail.com");
+
+            db.addCustomer( customer, account, address );
+            Customer customer2 = db.getCustomer(db.getLatestCustomerID());
+
+            //Console.WriteLine( customer2.ToString() );
+
+            //Console.WriteLine("\nUpdating Customer \n");
+            customer2.Email = "mbxmuk002@myuct.ac.za";
+            customer2.FirstName = "Lucky";
+            db.editCustomer(customer2);
+            //Console.WriteLine( db.getCustomer(db.getLatestCustomerID() ).ToString() ) ;
+
+            Console.WriteLine("\nPrinting Account for customer2");
+            Console.WriteLine(db.getAccountWithID(customer2));  
             
-            
+            Account ac = db.getAccountWithID( customer2 );
+            ac.AccountName = "Venda Langa";
+            ac.CreditBalance = 0;
+            db.editCustomerAccount(ac);
 
-            Customer customer = new Customer("990404", "Mukhethwa", "Muebi", "0608055010", "mbxmuk002@myuct.ac.za", 5, 4);
-            customer.AccountID = 3;
+            Console.WriteLine("\nPrinting Updated Account details for customer2");
+            Console.WriteLine(db.getAccountWithID(customer2));
 
-            db.addCustomer(customer);
-
-            Console.WriteLine("Added Employee");
-
-            Console.WriteLine("This is a test run");
-            Console.WriteLine(db.getCustomer(5));
-
-            Console.WriteLine("test if update works");
-            Customer customer2 = new Customer("990404", "Mukhethwa", "Muebi", "0608055010", "muebiml1999@gmail.com", 5, 4);
-            customer2.AccountID = 3;
-            db.updateCustomerDetails(customer2);
-            Console.WriteLine(db.getCustomer(5));
         }
     }
 }
