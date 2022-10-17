8e0acdaa9e709e0f79d36b136ac95564b77a3170
karabo-debug <82844238+karabo-debug@users.noreply.github.com>
2022-10-17 04:26:39 +0200

    Forms added to presentation layer

---
+++ Presentation Layer/CreateAnOrder.cs
@@ -0,0 +1,81 @@
+﻿/* Create an order form:
+ * it displays two options "New Customer" and "Existing Customer"
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoppelOrderingSystem_INF2011S_Project.Presentation_Layer
+{
+    public class CreateAnOrder : AccountDetailsForm
+    {
+        private System.Windows.Forms.Button newCustomerOption;
+        private System.Windows.Forms.Button existingCustomerButton;
+
+        public CreateAnOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.existingCustomerButton = new System.Windows.Forms.Button();
+            this.newCustomerOption = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // existingCustomerButton
+            // 
+            this.existingCustomerButton.Location = new System.Drawing.Point(231, 30);
+            this.existingCustomerButton.Name = "existingCustomerButton";
+            this.existingCustomerButton.Size = new System.Drawing.Size(99, 133);
+            this.existingCustomerButton.TabIndex = 1;
+            this.existingCustomerButton.Text = "Existing Customer";
+            this.existingCustomerButton.UseVisualStyleBackColor = true;
+            this.existingCustomerButton.Click += new System.EventHandler(this.existingCustomerButton_Click);
+            // 
+            // newCustomerOption
+            // 
+            this.newCustomerOption.Location = new System.Drawing.Point(86, 30);
+            this.newCustomerOption.Name = "newCustomerOption";
+            this.newCustomerOption.Size = new System.Drawing.Size(99, 133);
+            this.newCustomerOption.TabIndex = 2;
+            this.newCustomerOption.Text = "New Customer";
+            this.newCustomerOption.UseVisualStyleBackColor = true;
+            this.newCustomerOption.Click += new System.EventHandler(this.newCustomerOption_Click);
+            // 
+            // CreateAnOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(420, 240);
+            this.Controls.Add(this.newCustomerOption);
+            this.Controls.Add(this.existingCustomerButton);
+            this.Name = "CreateAnOrder";
+            this.Text = "Create a new order";
+            this.Load += new System.EventHandler(this.TemplateForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void TemplateForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void existingCustomerButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newCustomerOption_Click(object sender, EventArgs e)
+        {
+            // after choosing "New Customer" option open CustomerDetailsForm
+            CustomerDetailsForm customerDetailsForm = new CustomerDetailsForm();
+            customerDetailsForm.Show();
+
+        }
+    }
+}
---
+++ Presentation Layer/CustomerDetailsForm.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace PoppelOrderingSystem_INF2011S_Project.Presentation_Layer
+{
+    partial class CustomerDetailsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterDetailsLabel = new System.Windows.Forms.Label();
+            this.customerIDLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.phoneNumberLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.customerIDTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.streetNameTextBox = new System.Windows.Forms.TextBox();
+            this.townTextBox = new System.Windows.Forms.TextBox();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.postalCodeTextBox = new System.Windows.Forms.TextBox();
+            this.continueButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // enterDetailsLabel
+            // 
+            this.enterDetailsLabel.AutoSize = true;
+            this.enterDetailsLabel.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterDetailsLabel.Location = new System.Drawing.Point(196, 19);
+            this.enterDetailsLabel.Name = "enterDetailsLabel";
+            this.enterDetailsLabel.Size = new System.Drawing.Size(227, 26);
+            this.enterDetailsLabel.TabIndex = 0;
+            this.enterDetailsLabel.Text = "Enter customer details";
+            // 
+            // customerIDLabel
+            // 
+            this.customerIDLabel.AutoSize = true;
+            this.customerIDLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerIDLabel.Location = new System.Drawing.Point(100, 72);
+            this.customerIDLabel.Name = "customerIDLabel";
+            this.customerIDLabel.Size = new System.Drawing.Size(110, 21);
+            this.customerIDLabel.TabIndex = 1;
+            this.customerIDLabel.Text = "Customer ID:";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNameLabel.Location = new System.Drawing.Point(100, 105);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(97, 21);
+            this.firstNameLabel.TabIndex = 2;
+            this.firstNameLabel.Text = "First Name:";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNameLabel.Location = new System.Drawing.Point(100, 136);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(95, 21);
+            this.lastNameLabel.TabIndex = 3;
+            this.lastNameLabel.Text = "Last Name:";
+            // 
+            // phoneNumberLabel
+            // 
+            this.phoneNumberLabel.AutoSize = true;
+            this.phoneNumberLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneNumberLabel.Location = new System.Drawing.Point(100, 167);
+            this.phoneNumberLabel.Name = "phoneNumberLabel";
+            this.phoneNumberLabel.Size = new System.Drawing.Size(131, 21);
+            this.phoneNumberLabel.TabIndex = 4;
+            this.phoneNumberLabel.Text = "Phone Number:";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailLabel.Location = new System.Drawing.Point(100, 199);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(55, 21);
+            this.emailLabel.TabIndex = 5;
+            this.emailLabel.Text = "Email:";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressLabel.Location = new System.Drawing.Point(101, 232);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(74, 21);
+            this.addressLabel.TabIndex = 6;
+            this.addressLabel.Text = "Address:";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(237, 105);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(197, 20);
+            this.firstNameTextBox.TabIndex = 8;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(237, 136);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(197, 20);
+            this.lastNameTextBox.TabIndex = 9;
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(237, 167);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(197, 20);
+            this.phoneNumberTextBox.TabIndex = 10;
+            // 
+            // customerIDTextBox
+            // 
+            this.customerIDTextBox.Location = new System.Drawing.Point(237, 72);
+            this.customerIDTextBox.Name = "customerIDTextBox";
+            this.customerIDTextBox.Size = new System.Drawing.Size(197, 20);
+            this.customerIDTextBox.TabIndex = 11;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(237, 199);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(197, 20);
+            this.emailTextBox.TabIndex = 12;
+            // 
+            // streetNameTextBox
+            // 
+            this.streetNameTextBox.Location = new System.Drawing.Point(237, 232);
+            this.streetNameTextBox.Name = "streetNameTextBox";
+            this.streetNameTextBox.Size = new System.Drawing.Size(197, 20);
+            this.streetNameTextBox.TabIndex = 13;
+            // 
+            // townTextBox
+            // 
+            this.townTextBox.Location = new System.Drawing.Point(237, 258);
+            this.townTextBox.Name = "townTextBox";
+            this.townTextBox.Size = new System.Drawing.Size(197, 20);
+            this.townTextBox.TabIndex = 14;
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(237, 285);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(197, 20);
+            this.cityTextBox.TabIndex = 15;
+            // 
+            // postalCodeTextBox
+            // 
+            this.postalCodeTextBox.Location = new System.Drawing.Point(237, 312);
+            this.postalCodeTextBox.Name = "postalCodeTextBox";
+            this.postalCodeTextBox.Size = new System.Drawing.Size(197, 20);
+            this.postalCodeTextBox.TabIndex = 16;
+            // 
+            // continueButton
+            // 
+            this.continueButton.Location = new System.Drawing.Point(279, 395);
+            this.continueButton.Name = "continueButton";
+            this.continueButton.Size = new System.Drawing.Size(132, 27);
+            this.continueButton.TabIndex = 19;
+            this.continueButton.Text = "Continue";
+            this.continueButton.UseVisualStyleBackColor = true;
+            this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(362, 353);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(123, 26);
+            this.submitButton.TabIndex = 20;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(214, 353);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(117, 26);
+            this.cancelButton.TabIndex = 21;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click_1);
+            // 
+            // CustomerDetailsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 434);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.postalCodeTextBox);
+            this.Controls.Add(this.cityTextBox);
+            this.Controls.Add(this.townTextBox);
+            this.Controls.Add(this.streetNameTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.customerIDTextBox);
+            this.Controls.Add(this.phoneNumberTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.phoneNumberLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.customerIDLabel);
+            this.Controls.Add(this.enterDetailsLabel);
+            this.Name = "CustomerDetailsForm";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.CustomerDetailsForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label enterDetailsLabel;
+        private System.Windows.Forms.Label customerIDLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label phoneNumberLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox customerIDTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox streetNameTextBox;
+        private System.Windows.Forms.TextBox townTextBox;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.TextBox postalCodeTextBox;
+        private System.Windows.Forms.Button continueButton;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
