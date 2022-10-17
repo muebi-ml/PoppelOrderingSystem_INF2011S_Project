597b86c82aaddc0f949e617c1aabc9c667250a22
Mukhethwa Muebi <MBXMUK002@myuct.ac.za>
2022-10-17 12:03:39 +0200

    fix program.cs merge conflicts

--- Program.cs
+++ Program.cs
@@ -20,58 +20,8 @@ namespace PoppelOrderingSystem_INF2011S_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
-            Application.Run(new OrderParentForm()); // form test run
-/*
-<<<<<<< HEAD
-=======
-=======
-            //Application.Run(new Form1());
+            Application.Run(new OrderParentForm());
 
->>>>>>> e4fecefbf2141cf2c9cb477481c83e1a12a083d0
-
-            /*CustomerDB db = new CustomerDB();
-            
-            
-
-
-            CustomerDB customerDB = new CustomerDB();
-
-            Customer customer = customerDB.getCustomer(4785300);
-
-            Console.WriteLine(customer);
-
-            Console.WriteLine(customerDB.getAccountWithID(customer));
-
-
-            Console.WriteLine("test if update works");
-            Customer customer2 = new Customer("990404", "Mukhethwa", "Muebi", "0608055010", "muebiml1999@gmail.com", 5, 4);
-            customer2.AccountID = 3;
-            db.updateCustomerDetails(customer2);
-            Console.WriteLine(db.getCustomer(2)); 
-
-
-            // Test run to display all products
-            OrderDB order = new OrderDB();
-
-            Console.WriteLine("Show all products");
-            Console.WriteLine();
-            order.DisplayProducts();
-
-            // Test run to add Order
-            
-            Order ord = new Order(10200,"2022/03/12","2022/04/12","2022/04/12",Order.Status.In_Process,3);
-
-            order.addOrder(ord);
-
-            Console.WriteLine(order.getOrderByOrderNumber(10200));
-
-            
-<<<<<<< HEAD
->>>>>>> karabo-code **/
-=======
-
->>>>>>> e4fecefbf2141cf2c9cb477481c83e1a12a083d0
         }
     }
 }
